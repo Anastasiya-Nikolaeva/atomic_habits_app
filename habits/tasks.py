e3dc7faa7f9055ed8657e7a10ec3530d00249a56
Anastasiya Nikolaeva <eritreya666@gmail.com>
2025-04-29 20:44:49 +0300

    update project

---
+++ habits/tasks.py
@@ -0,0 +1,34 @@
+from celery import shared_task
+from drf_yasg.utils import logger
+
+from habits.models import Habit
+from habits.services import send_telegram_message
+
+
+@shared_task
+def send_message_to_user():
+    """
+    Отправка уведомлений о выполнении привычки
+    """
+    habits = Habit.objects.filter(sign_of_a_pleasant_habit=False)
+    for habit in habits:
+        habit.send_indicator -= 1
+
+        if habit.send_indicator <= 0:  # Проверка на ноль или меньше
+            if habit.owner.tg_chat_id:
+                message = (
+                    f"У вас сегодня выполнение привычки: {habit.habit}, "
+                    f"которую нужно выполнить в {habit.time_execution} "
+                    f"в {habit.place_of_execution}"
+                )
+                try:
+                    send_telegram_message(message=message, chat_id=habit.owner.tg_chat_id)
+                except Exception as e:
+                    # Логирование ошибки
+                    logger.error(f"Ошибка при отправке сообщения: {e}")
+                else:
+                    habit.send_indicator = habit.periodicity  # Сброс индикатора
+            else:
+                logger.warning(f"У пользователя {habit.owner.id} отсутствует tg_chat_id.")
+
+        habit.save(update_fields=["send_indicator"])
